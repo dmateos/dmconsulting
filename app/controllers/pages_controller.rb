6976a1c0c75e413241e3710eb3b05e3945a11c84
Daniel Mateos <daniel@mateos.cc>
2014-11-11 14:44:54 +1030

    more theme work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController < ApplicationController
   def index
     
   end
+
+  def about
+
+  end
+
+  def services
+
+  end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#index'
+  get 'about', to: 'pages#about'
+  get 'services', to: 'pages#services'
+  get 'contact', to: 'pages#contact'
 end
