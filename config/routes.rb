a22a17f21c41b9844690d85720aa272c484350cd
Daniel Mateos <daniel@mateos.cc>
2014-12-13 21:42:30 +1030

    contacts model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root :to => 'pages#index'
   get 'about', to: 'pages#about'
   get 'services', to: 'pages#services'
-  get 'contact', to: 'pages#contact'
+
+  resource :contacts
 end
