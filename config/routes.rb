aae094e3ec52a2d6c882747c5d4ce148ffe634a3
Daniel Mateos <daniel@mateos.cc>
2015-01-05 00:09:57 +1030

    4.2 upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root :to => 'pages#index'
-
+  root to: 'pages#index'
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
   get 'services', to: 'pages#services'
